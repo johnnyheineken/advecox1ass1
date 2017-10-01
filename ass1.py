efc6c53b84cec7428cee771cc3be780b6cf7fb47
johnnyheineken <janhynek@gmail.com>
2017-10-01 23:28:30 +0200

    made functions
    a functionalised it a bit

--- ass1.py
+++ ass1.py
@@ -16,26 +16,37 @@ print('This is the Python output for Assignment 1, Advanced Econometrics 1 for s
       '   (2) ' + studentname2 + ' - ' + str(studentnumber2) + '\n\n' +
       '   Group n')
 
-N = 20
+N = 50
 r = 3
 beta = 1
-cov_matrix2 = np.identity(r + 2) * sigma_z
-cov_matrix2[:, 0] = (r + 2) * [gamma]
-cov_matrix2[0, :] = (r + 2) * [gamma]
-cov_matrix2[0, 0] = 1
-cov_matrix2[1, 1] = 1
-cov_matrix2[0, 1] = ro
-cov_matrix2[1, 0] = ro
-print(cov_matrix2)
 
+
+sigma_z = 2
+gamma = 0.50
+ro = 0.5
+
+
+def cov_matrix(sigma_z, gamma, ro):
+    cov_matrix = np.identity(r + 2) * sigma_z
+    cov_matrix[:, 0] = (r + 2) * [gamma]
+    cov_matrix[0, :] = (r + 2) * [gamma]
+    cov_matrix[0, 0] = 1
+    cov_matrix[1, 1] = 1
+    cov_matrix[0, 1] = ro
+    cov_matrix[1, 0] = ro
+    return cov_matrix
 
 
 # defined as three values = [u, v, epsilon]
 
-errors_vector = np.random.multivariate_normal(mean=[0, 0, 0],
-                                              cov=cov_matrix,
-                                              size=N)
+def errors(cov_matrix):
+    errors_vector = np.random.multivariate_normal(mean=[0] * (r + 2),
+                                                  cov=cov_matrix,
+                                                  size=N)
+    return errors_vector
 
-z = errors_vector[:,2]
 
-print(z)
+z = errors(cov_matrix(sigma_z, gamma, ro))[:, 2:]
+
+
+
