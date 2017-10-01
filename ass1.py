1640573299a6e57b8a81352f7c7fdbf8cfbe7042
johnnyheineken <janhynek@gmail.com>
2017-10-01 20:58:34 +0200

    New script
    I added basic information, required for the assignment

---
+++ ass1.py
@@ -0,0 +1,21 @@
+import numpy as np
+from sklearn import linear_model
+import math
+from numpy.linalg import *
+from numpy import transpose as t
+import matplotlib.pyplot as plt
+
+
+studentname1 = 'Jan Hynek'
+studentname2 = 'Štěpán Svoboda'
+studentnumber1 = 1189
+studentnumber2 = 1188  # ŠTĚPÁNE NEZAPOMEŃ ZMĚNIT ČÍSLO
+np.random.seed(studentnumber1 + 10 * 1488)
+print('This is the Python output for Assignment 1, Advanced Econometrics 1 for students: \n' +
+      '   (1) ' + studentname1 + ' - ' + str(studentnumber1) + '\n' +
+      '   (2) ' + studentname2 + ' - ' + str(studentnumber2) + '\n\n' +
+      '   Group n')
+
+N = 20
+r = 3
+beta = 1
