971b831c39d2a6c9e5a33965f08e196d9fc03d52
johnnyheineken <janhynek@gmail.com>
2017-10-02 23:38:51 +0200

    outcome function
    outcome function added

--- ass1.py
+++ ass1.py
@@ -123,8 +123,7 @@ def iteration(gamma, ro, pi_1, N_obs, sigma_z, r):
 
     # formatting results:
     result = (b_hat_ols[0, 0], b_hat_2sls[0, 0],
-              Var_b_ols[0, 0], Var_b_2sls[0, 0], (pi.T * Z.T * Z * pi) /
-              (np.sum(np.power(error_matrix[:, 1], 2)) / N_obs))
+              Var_b_ols[0, 0], Var_b_2sls[0, 0], (pi.T * Z.T * Z * pi))
     return [np.around(i, 3) for i in result]
     # print('b_hat_ols:  ' + str(b_hat_ols))
     # print('b_hat_2sls: ' + str(b_hat_2sls))
@@ -156,27 +155,53 @@ def monte_carlo(gamma, ro, pi_1, n_iterations, N_obs, sigma_z, r):
 
 #%%
 
-Hausman_res1, parameters = monte_carlo(gamma=0,
-                                       ro=0.5,
-                                       pi_1=0.4,
-                                       n_iterations=100,
-                                       N_obs=100,
-                                       sigma_z=2,
-                                       r=3)
+def outcome(gamma, ro, pi_1, n_iterations, N_obs, sigma_z, r):
+    Hausman_res, parameters = monte_carlo(gamma, ro, pi_1, n_iterations, N_obs, sigma_z, r)
+    used_pars=pd.DataFrame.from_items([('parameters:', parameters)], orient='index',
+                                        columns=['gamma', 'ro', 'pi_1',
+                                                 'n_iterations', 'N_obs',
+                                                 'sigma_z', 'r'])
+    print(used_pars)
+    positive_res=len(
+        np.where(Hausman_res[:, 0] > stats.chi2.ppf(0.95, 1))[0])
+
+    print('\nThe Hausman test passed the critical value ' + str(positive_res) +
+          ' times out of ' + str(parameters[3]) + ' iterations')
+    print('The average concentration parameter is ' +
+          str(np.mean(Hausman_res[:, 2])))
+
+    plt.hist(Hausman_res[:, 0], bins=40)
+    plt.show()
 
 
-used_pars = pd.DataFrame.from_items([('parameters:', parameters)], orient='index',
-                                    columns=['gamma', 'ro', 'pi_1',
-                                             'n_iterations', 'N_obs',
-                                             'sigma_z', 'r'])
-print(used_pars)
-positive_res = len(
-    np.where(Hausman_res1[:, 0] > stats.chi2.ppf(0.95, 1))[0])
+outcome(gamma=0,
+        ro=0.2,
+        pi_1=0.5,
+        n_iterations=500,
+        N_obs=50,
+        sigma_z=2,
+        r=3)
 
-print('\nThe Hausman test passed the critical value ' + str(positive_res) +
-      ' times out of ' + str(parameters[3]) + ' iterations')
-print('The average concentration parameter is ' +
-      str(np.mean(Hausman_res1[:, 2])))
 
-plt.hist(Hausman_res1[:, 0], bins=40)
-plt.show()
+
+#%%
+
+
+np.array([[r, N_obs, ro, gamma, pi_1],
+          [3, 50, 0, 0, 0.5], # realistic assumption is we study overidentified case, let's keep 3
+          [1, 50, 0, 0, 0.5],
+          [5, 50, 0, 0, 0.5],
+          [3, 50, 0, 0, 0.1], # weak instrument. big variance, very few rejections
+          [3, 100, 0, 0, 0.5], # more observations means higher concentration parameter
+          [3, 100, 0.25, 0, 0.1],
+          [3, 100, 0.25, 0, 0.25],
+          [3, 100, 0.25, 0, 0.5],
+          [3, 100, 0.5, 0, 0.1],
+          [3, 100, 0.5, 0, 0.25],
+          [3, 100, 0.5, 0, 0.5],
+          [3, 100, 0.25, 0.1, 0.1], # show the risks of gamma > 0
+          [3, 100, 0.25, 0.1, 0.25], # if i were to assume z is exogenous then i would come to the conclusion x is exogenous as well
+          [3, 100, 0.25, 0.1, 0.5]]) # i cannot say anything
+    
+    
+    
--- ass1_old.py
+++ ass1_old.py
@@ -1,3 +1,13 @@
+#%%
+
+'''
+import IPython
+app = IPython.Application.instance()
+app.kernel.do_shutdown(True)
+'''
+
+#%%
+
 import numpy as np
 from sklearn import linear_model
 import math
@@ -5,7 +15,11 @@ from numpy.linalg import *
 from numpy import transpose as t
 from itertools import chain
 import matplotlib.pyplot as plt
+from scipy import stats as stats
+import pandas as pd
 
+
+#%%
 
 studentname1 = 'Jan Hynek'
 studentname2 = 'Štěpán Svoboda'
@@ -69,10 +83,8 @@ def iteration(gamma, ro, pi_1, N_obs, sigma_z, r):
     ##########################
     # Ordinary least squares #
     ##########################
-    # we do not fit intercept
-    # as we know that our original model does not have intercept.
-    # however, we think that in monte carlo simulation 
-    # it wont make a difference if the intercept would be zero
+    # we do not fit intercept as we know that our original model does not have intercept.
+    # however, we think that in monte carlo simulation it wont make a difference if the intercept would be zero
     # as it is estimated as almost zero
     regression_ols = linear_model.LinearRegression(fit_intercept=False)
     regression_ols.fit(X=X, y=y)
@@ -111,21 +123,33 @@ def iteration(gamma, ro, pi_1, N_obs, sigma_z, r):
 
     # formatting results:
     result = (b_hat_ols[0, 0], b_hat_2sls[0, 0],
-              Var_b_ols[0, 0], Var_b_2sls[0, 0])
-    return [np.round(i, 3) for i in result]
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+              Var_b_ols[0, 0], Var_b_2sls[0, 0], (pi.T * Z.T * Z * pi))
+=======
+              Var_b_ols[0, 0], Var_b_2sls[0, 0], pi.T * Z.T * Z * pi)
+>>>>>>> 1c2fb5221b749c5adf6c51d7669138d248b7eabb
+=======
+              Var_b_ols[0, 0], Var_b_2sls[0, 0], pi.T * Z.T * Z * pi)
+>>>>>>> 1c2fb5221b749c5adf6c51d7669138d248b7eabb
+=======
+              Var_b_ols[0, 0], Var_b_2sls[0, 0], pi.T * Z.T * Z * pi)
+>>>>>>> 1c2fb5221b749c5adf6c51d7669138d248b7eabb
+    return [np.around(i, 3) for i in result]
     # print('b_hat_ols:  ' + str(b_hat_ols))
     # print('b_hat_2sls: ' + str(b_hat_2sls))
     # print('Var_b_ols:  ' + str(Var_b_ols))
     # print('Var_b_2sls: ' + str(Var_b_2sls))
 
 
-
+#%%
 
 def monte_carlo(gamma, ro, pi_1, n_iterations, N_obs, sigma_z, r):
-    results_Hausman = np.zeros((n_iterations, 1))
+    results_Hausman = np.zeros((n_iterations, 3))
     for i in range(n_iterations):
-        (b_hat_ols, b_hat_2sls,
-         Var_b_ols, Var_b_2sls) = iteration(gamma, ro, pi_1, N_obs, sigma_z, r)
+        (b_hat_ols, b_hat_2sls, Var_b_ols, Var_b_2sls, conc_par) = iteration(
+            gamma, ro, pi_1, N_obs, sigma_z, r)
         d_Var = Var_b_2sls - Var_b_ols
         if d_Var == 0:
             d_Var_psinv = 0
@@ -133,17 +157,95 @@ def monte_carlo(gamma, ro, pi_1, n_iterations, N_obs, sigma_z, r):
             d_Var_psinv = 1 / d_Var
 
         Hausman = ((b_hat_ols - b_hat_2sls)) ** 2 * d_Var_psinv
-        results_Hausman[i, :] = Hausman
-    return results_Hausman
+        results_Hausman[i, 0] = Hausman
+        df = b_hat_2sls[np.abs(b_hat_2sls) < 1e8].size
+        pval = stats.chi2.sf(Hausman, df)
+        results_Hausman[i, 1] = pval
+        results_Hausman[i, 2] = conc_par
+    return results_Hausman, [gamma, ro, pi_1, n_iterations, N_obs, sigma_z, r]
 
 
-Hausman_res = monte_carlo(gamma=0.5,
-                          ro=0.4,
-                          pi_1=0.6,
-                          n_iterations=200,
-                          N_obs=1000,
-                          sigma_z=2,
-                          r=3)
+#%%
+<<<<<<< HEAD
+def outcome(gamma, ro, pi_1, n_iterations, N_obs, sigma_z, r):
+    Hausman_res, parameters = monte_carlo(gamma, ro, pi_1, n_iterations, N_obs, sigma_z, r)
+    used_pars=pd.DataFrame.from_items([('parameters:', parameters)], orient='index',
+                                        columns=['gamma', 'ro', 'pi_1',
+                                                 'n_iterations', 'N_obs',
+                                                 'sigma_z', 'r'])
+    print(used_pars)
+    positive_res=len(
+        np.where(Hausman_res[:, 0] > stats.chi2.ppf(0.95, 1))[0])
 
-plt.hist(Hausman_res, bins=40)
+    print('\nThe Hausman test passed the critical value ' + str(positive_res) +
+          ' times out of ' + str(parameters[3]) + ' iterations')
+    print('The average concentration parameter is ' +
+          str(np.mean(Hausman_res[:, 2])))
+
+    plt.hist(Hausman_res[:, 0], bins=40)
+    plt.show()
+
+
+outcome(gamma=0,
+        ro=0.2,
+        pi_1=0.5,
+        n_iterations=500,
+        N_obs=50,
+        sigma_z=2,
+        r=3)
+=======
+
+Hausman_res1, parameters = monte_carlo(gamma=0,
+                                       ro=0.5,
+                                       pi_1=0.4,
+                                       n_iterations=100,
+                                       N_obs=100,
+                                       sigma_z=2,
+                                       r=3)
+
+
+used_pars = pd.DataFrame.from_items([('parameters:', parameters)], orient='index',
+                                    columns=['gamma', 'ro', 'pi_1',
+                                             'n_iterations', 'N_obs',
+                                             'sigma_z', 'r'])
+print(used_pars)
+positive_res = len(
+    np.where(Hausman_res1[:, 0] > stats.chi2.ppf(0.95, 1))[0])
+
+print('\nThe Hausman test passed the critical value ' + str(positive_res) +
+      ' times out of ' + str(parameters[3]) + ' iterations')
+print('The average concentration parameter is ' +
+      str(np.mean(Hausman_res1[:, 2])))
+
+plt.hist(Hausman_res1[:, 0], bins=40)
 plt.show()
+
+
+#%%
+
+
+np.array([[r, N_obs, ro, gamma, pi_1],
+          [3, 50, 0, 0, 0.5], # realistic assumption is we study overidentified case, let's keep 3
+          [1, 50, 0, 0, 0.5],
+          [5, 50, 0, 0, 0.5],
+          [3, 50, 0, 0, 0.1], # weak instrument. big variance, very few rejections
+          [3, 100, 0, 0, 0.5], # more observations means higher concentration parameter
+          [3, 100, 0.25, 0, 0.1],
+          [3, 100, 0.25, 0, 0.25],
+          [3, 100, 0.25, 0, 0.5],
+          [3, 100, 0.5, 0, 0.1],
+          [3, 100, 0.5, 0, 0.25],
+          [3, 100, 0.5, 0, 0.5],
+          [3, 100, 0.25, 0.1, 0.1], # show the risks of gamma > 0
+          [3, 100, 0.25, 0.1, 0.25], # if i were to assume z is exogenous then i would come to the conclusion x is exogenous as well
+          [3, 100, 0.25, 0.1, 0.5]]) # i cannot say anything
+    
+    
+    
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 1c2fb5221b749c5adf6c51d7669138d248b7eabb
+=======
+>>>>>>> 1c2fb5221b749c5adf6c51d7669138d248b7eabb
+=======
+>>>>>>> 1c2fb5221b749c5adf6c51d7669138d248b7eabb
